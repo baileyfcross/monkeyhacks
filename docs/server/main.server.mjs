5c8c5b219cd17a02a9a9cbc89d21cdb8462f9c6c
baileyfcross <bf02cros@siena.edu>
2025-02-22 02:03:57 -0500

    Changed dir and added build commands for docs folder

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BVSYWEYE.mjs";import"./chunk-YVURL6XR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-MDHMVL6Y.mjs";import"./chunk-M26RYH5B.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
